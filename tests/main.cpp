6411dc4ee2451cafe7363816539ba2b0c5a1b332
vaeleborn <dmorgan98101+gmail.com>
2025-03-19 22:24:15 -0700

    Added a cross platform pause function to the end of our tests.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,16 @@
 #include <gtest/gtest.h>
+#include <iostream>
+void Pause()
+{
+    std::cout << "\nPress Any Key To Continue...\n";
+    std::cin.get();
+}
 
 //Entry Point for Google Test
 int main(int argc, char** argv) 
 {
     ::testing::InitGoogleTest(&argc, argv);
     int tests = RUN_ALL_TESTS();
-    system("Pause");
+    Pause();
     return tests;
 }
